541e6d35289e8b36960dd2fa0b2b8fae6e0d7db8
Daryl Halima <dhalima@gatech.edu>
2014-02-21 17:40:35 -0500

    Update StudentOrgManagerContract.java

--- StudentOrgManagerContract.java
+++ StudentOrgManagerContract.java
@@ -1,8 +1,8 @@
-
 public final class StudentOrgManagerContract {
-	public StudentOrgManagerContract(){}
-	
-	public static abstract class StudentOrgManager implements BaseColumns {
+	public StudentOrgManagerContract() {
+	}
+
+	public static abstract class Users implements BaseColumns {
 		public static final String TABLE_NAME = "users";
 		public static final String COLUMN_NAME_USERNAME = "username";
 		public static final String COLUMN_NAME_PASSWORD = "password";
@@ -14,5 +14,44 @@ public final class StudentOrgManagerContract {
 		public static final String COLUMN_NAME_GRADUATIONYEAR = "graduationyear";
 		public static final String COLUMN_NAME_BIO = "bio";
 		public static final String COLUMN_NAME_PICTUREREF = "pictureref";
+	}
+	
+	public static abstract class UserOrgs implements BaseColumns {
+		public static final String TABLE_NAME = "user_orgs";
+		public static final String COLUMN_NAME_USERNAME = "username";
+		public static final String COLUMN_NAME_ORGANIZATION = "organization";
+		public static final String COLUMN_NAME_POSITION = "position";
+	}
+
+	public static abstract class Organizations implements BaseColumns {
+		public static final String TABLE_NAME = "organizations";
+		public static final String COLUMN_NAME_ORGNAME = "name";
+		public static final String COLUMN_NAME_TYPE = "type";
+		public static final String COLUMN_NAME_SIZE = "size";
+	}
+
+	public static abstract class NewsItems implements BaseColumns {
+		public static final String TABLE_NAME = "newsitems";
+		public static final String COLUMN_NAME_ORGANIZATION = "organization";
+		public static final String COLUMN_NAME_NEWSTIMESTAMP = "newstimestamp";
+		public static final String COLUMN_NAME_ANNOUNCEMENT = "announcement";
+		public static final String COLUMN_NAME_POSTER = "poster";
+	}
+
+	public static abstract class Events implements BaseColumns {
+		public static final String TABLE_NAME = "events";
+		public static final String COLUMN_NAME_EVENTID = "id";
+		public static final String COLUMN_NAME_ORGANIZATION = "organization";
+		public static final String COLUMN_NAME_EVENTDATETIME = "event_datetime";
+		public static final String COLUMN_NAME_LOCATION = "location";
+		public static final String COLUMN_NAME_DESCRIPTION = "description";
+		public static final String COLUMN_NAME_TYPE = "type";
+	}
+	
+	public static abstract class Absences implements BaseColumns {
+		public static final String TABLE_NAME = "absences";
+		public static final String COLUMN_NAME_USERNAME = "username";
+		public static final String COLUMN_NAME_ORGANIZATION = "organization";
+		public static final String COLUMN_NAME_EVENTID = "eventid";
 	}
 }
