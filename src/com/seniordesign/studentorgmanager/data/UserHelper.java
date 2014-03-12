440ddfca466a7218c38e2093c3a6cc5e25f17476
Sanchit Malhotra <smalhotra7@gatech.edu>
2014-03-12 02:36:14 -0400

    Added new folders (data and model), Added a login screen, added a few model classes and a skeleton for one of the helper classes (UserHelper.java)

--- src/com/seniordesign/studentorgmanager/data/UserHelper.java
+++ src/com/seniordesign/studentorgmanager/data/UserHelper.java
@@ -4,4 +4,15 @@ package com.seniordesign.studentorgmanager.data;
 
 public class UserHelper {
 
+	/**
+	 * Checks if the username/password combo is valid
+	 * @param username
+	 * @param password
+	 * @return if it is valid
+	 */
+	public static boolean authenticate(String username, String password) {
+		//TODO
+		
+		return true;
+	}
 }
